fbc5084998040a162ccd5cc9290820c6fc82ac79
nicolas.carrara <nicolas.carrara1u@gmail.com>
2018-12-27 22:50:46 +0100

    modify setup

---
+++ clean.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/sh
+rm -rf pydial
+rm -rf build
+rm -rf __pycach*
+rm -rf dist
+rm -rf *egg*
+rm auto
+rm -rf belieftracking
+rm -rf cedm
+rm -rf config
+rm -rf Docs
+rm -rf evaluation
+rm -rf ontology
+rm -rf policy
+rm -rf scripts
+rm -rf semanticbelieftracking
+rm -rf semi
+rm -rf semo
+rm -rf tasks
+rm -rf tests
+rm -rf topictracking
+rm -rf usersimulator
+rm -rf utils
+rm Agent.py
+rm conf.py
+rm DialogueServer.py
+rm Simulate.py
+rm Texthub.py
+rm run.log
+rm pydial.py
