d0073dc959747ced36c5e8e31f0821abd55f2bd7
nicolas.carrara <nicolas.carrara1u@gmail.com>
2018-12-27 23:01:11 +0100

    oka

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # cp this file to pydial git folder, then use the command "python setup.py install" in the shell.
 
 setup(
-    name = "gym-pydial",
+    name = "gympydial",
     version = "0.0.0",
     description = ("Gym wrapper for pydial"),
     packages=find_packages(),
