5953b765b2ae2db46cac2bf262045aa933051e4b
nicolas.carrara <nicolas.carrara1u@gmail.com>
2018-12-28 13:06:19 +0100

    gogogo

--- create_sources.sh
+++ create_sources.sh
@@ -18,7 +18,7 @@ sed -i 's/import dact, copy/from \. import dact\nimport copy/' utils/DiaAct.py
 find ./ -type f -exec sed -i -e 's/from \. import PER\.sum_tree/import policy\.DRL\.PER/g' {} \;
 find ./ -type f -exec sed -i -e 's/from \. import DRL/import policy\.DRL/g' {} \;
 sed -i 's/from \. import trpo_utils\.distribution\.utils as utils/import policy\.DRL\.trpo_utils\.distribution\.utils as utils/' policy/DRL/na2c.py
-
+sed -i 's/fin = file/fin = open/' semo/RNNLG/utils/nlp/nlp.py
 
 #sed -i 's/from \. import utils\.ContextLogger as clog/import utils\.ContextLogger as clog/' pydial.py
 #sed -i 's/from \. import usersimulator\.textgenerator\.textgen_toolkit\.SCTranslate as SCT/import usersimulator\.textgenerator\.textgen_toolkit\.SCTranslate as SCT/' Simulate.py
