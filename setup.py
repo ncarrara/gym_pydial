e6b99ab3ee2e85c4744dfdb2ecccaf04303feec4
nicolas.carrara <nicolas.carrara1u@gmail.com>
2018-12-28 14:32:39 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 # cp this file to pydial git folder, then use the command "python setup.py install" in the shell.
 
-setup(name='gympydial5',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+setup(name='gympydial',
+      version='0.0.0',
+      description='gym wrapper for single domain pydial',
+      url='https://github.com/ncarrara/gym_pydial',
+      author='Nicolas Carrara',
+      author_email='nicolas.carrara1u@gmail.com',
       license='MIT',
       packages=['gym_pydial'],
       zip_safe=False)
